7c773a6a4763aa818f27400a6191930b73f71b63
drilon305 <drilonsulejmani305@gmail.com>
2023-03-07 23:06:49 +0100

    finished header

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,10 +18,32 @@ const Header = () => {
         header ? "bg-white py-6 shadow-lg" : "bg-transparent py-8"
       } fixed z-50 w-full transition-all duration-500`}
     >
-      <div className="container mx-auto">
+      <div className="container mx-auto flex flex-col items-center gap-y-6 lg:flex-row lg:justify-between lg:gap-y-0">
         <a href="/">
-          {header ? <img src={LogoDark} className='w[160px]' /> : <img src={LogoWhite} className='w[160px]' />}
+          {header ? (
+            <img src={LogoDark} className="w[160px]" />
+          ) : (
+            <img src={LogoWhite} className="w[160px]" />
+          )}
         </a>
+        <nav className={`${header ? "text-primary" : "text-white"} flex gap-x-4 font-tertiary
+         tracking-[3px] text-[15px] items-center uppercase lg:gap-x-8`}>
+          <a href="/" className="hover:text-accent transition">
+            Home
+          </a>
+          <a href="/" className="hover:text-accent transition">
+            Rooms
+          </a>
+          <a href="/" className="hover:text-accent transition">
+            Restaurant
+          </a>
+          <a href="/" className="hover:text-accent transition">
+            Spa
+          </a>
+          <a href="/" className="hover:text-accent transition">
+            Contact
+          </a>
+        </nav>
       </div>
     </header>
   );
