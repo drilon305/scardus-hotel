dfdee85f4d2efadf680a97490e6c74e2320f540b
drilon305 <drilonsulejmani305@gmail.com>
2023-03-13 20:22:55 +0100

    designing roomdetails

--- src/pages/RoomDetails.js
+++ src/pages/RoomDetails.js
@@ -23,43 +23,54 @@ const RoomDetails = () => {
  
   const { name, description, facilities, price, imageLg} = room;
 
-  return <section>
-    <div className='bg-room bg-cover bg-center h-[560px] relative flex justify-center items-center'>
-      <div className="absolute w-full h-full bg-black/70 "></div>
-      <h1 className='text-6xl text-white z-20 font-primary text-center'>{name} Details</h1>
-    </div>
-    <div className="container mx-auto">
-      <div className="flex flex-col lg:flex-row h-full py-24">
+  return (
+    <section>
+      <div className="bg-room bg-cover bg-center h-[560px] relative flex justify-center items-center">
+        <div className="absolute w-full h-full bg-black/70 "></div>
+        <h1 className="text-6xl text-white z-20 font-primary text-center">
+          {name} Details
+        </h1>
+      </div>
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row h-full py-24">
+          {/* Left */}
+          <div className="w-full h-full lg:w-[60%] px-6 ">
+            <h2 className="h2">{name}</h2>
+            <p className="mb-8">{description}</p>
+            <img className="mb-8" src={imageLg} alt="/" />
+            <div className="mt-12">
+              <h3 className="h3 mb-3">Room Facilities</h3>
+              <p className="mb-12">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Mollitia saepe sequi quia natus explicabo aliquam repudiandae
+                repellat harum qui est, quo labore expedita, adipisci, nostrum
+                vero dignissimos dicta voluptates minima.
+              </p>
 
-        {/* Left */}
-        <div className="w-full h-full lg:w-[60%] px-6 bg-yellow-300">
-        <h2 className='h2'>{name}</h2>
-        <p className='mb-8'>{description}</p>
-        <img className='mb-8' src={imageLg} alt="/" />
-        <div className='mt-12'>
-          <h3 className='h3 mb-3'>Room Facilities</h3>
-          <p className='mb-12'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia saepe sequi quia natus explicabo aliquam repudiandae repellat harum qui est,
-             quo labore expedita, adipisci, nostrum vero dignissimos dicta voluptates minima.</p>
-
-        {/* Grid */}
-        <div>
-          {facilities.map((item, index) => {
-            return <div>
-              item
+              {/* Grid */}
+              <div className='grid grid-cols-3 gap-6 mb-12'>
+                {facilities.map((item, index) => {
+                  const { name, icon } = item;
+                  return (
+                  <div className='flex items-center gap-x-3 flex-1' key={index}>
+                    <div className='text-3xl text-accent'>{icon}</div>
+                    <div className='text-base'>{name}</div>
+                  </div>
+                )
+                })}
+              </div>
             </div>
-          })}
-        </div>
-        </div>
-        </div>
+          </div>
 
-        {/* Right */}
-        <div className="w-full h-full lg:w-[40%] bg-blue-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quos ab distinctio quis
-           neque ullam.
+          {/* Right */}
+          <div className="w-full h-full lg:w-[40%] bg-blue-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quos
+            ab distinctio quis neque ullam.
+          </div>
         </div>
       </div>
-    </div>
-  </section>;
+    </section>
+  );
 };
 
 export default RoomDetails;
